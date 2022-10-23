d32292290fda956fb49a339c4580c4069f48301d
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-23 07:25:29 -0300

    Update painel.sh

--- painel.sh
+++ painel.sh
@@ -7,6 +7,7 @@ termux-setup-storage;
 pip3 install pyaes;
 pip3 install keyboard;
 cd storage;
+cd downloads;
 git clone https://github.com/m7codee/painel_de_buscas;
 cd painel_de_buscas;
 python3 painel.py;
