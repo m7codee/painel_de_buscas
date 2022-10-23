764a0d12658c057c32e86338d212b0de6edabe5e
m7codee <104729995+m7codee@users.noreply.github.com>
2022-10-23 05:36:45 -0300

    Add files via upload

---
+++ painel.sh
@@ -0,0 +1,11 @@
+echo Painel de consultas privado;
+apt update;
+apt upgrade -y;
+apt install git -y;
+apt install python3 -y;
+termux-setup-storage;
+pip3 install pyaes;
+cd storage/downloads;
+git clone https://github.com/m7codee/painel_de_buscas;
+cd painel_de_buscas;
+python3 painel.py;
